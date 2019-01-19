a0b1961fc9da1e8b9d38988b337265b7015d7f3b
Михаил <michaeldeveloper01@gmail.com>
2019-01-19 20:07:48 +0200

    Доработал карусель для больших разрешений (прокрутка по три пункта списка). Индикатор не трогал.

--- js/carusel.js
+++ js/carusel.js
@@ -1,19 +1,17 @@
-var list = document.getElementById('carusel-list');
 
 function shiftList(list) {
 	list.style.marginLeft = "-82.5vw";
 }
 
-function transpositionItem(list) {
-	var item = list.childNodes[0];
-	list.appendChild(item);
+function transpositionItemsGroup(list) {
+	var itemsGroup = list.children[0];
+	list.appendChild(itemsGroup);
 }
 
 function makeCarusel() {
+	var list = document.getElementById('carusel-list');
 	shiftList(list);
-	transpositionItem(list);
-	transpositionItem(list);
-	transpositionItem(list);
+	transpositionItemsGroup(list);
 }
 
 setInterval(makeCarusel, 3000);
